e8429e8513677d3561f3eb7d7bcc4660b93c8664
Ismael E. Lastre Alvarez <ismaelfac@gmail.com>
2017-11-11 23:56:22 -0500

    Modificado app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Ismaelfac</h1>
+          <h1 className="App-title">Welcome to Ismaelfac@gmail.com</h1>
           <h4 className="">Excelente proyecto</h4>
         </header>
         <p className="App-intro">
